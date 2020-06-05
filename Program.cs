e81b80738039ff7b5575d3b965f4fab1f1d24b75
KanXxw46 <1kan_andreikkkk@mail.ru>
2020-06-05 21:14:28 +0600

    Practic

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeWork6_Practic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
